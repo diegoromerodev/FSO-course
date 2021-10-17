3775f8b294692aee9354ab8ae38bb50328514293
Diego Romero <diegoromeropersonal@gmail.com>
2021-10-16 22:53:23 -0300

    Static dir

--- part3/phonebook-back/index.js
+++ part3/phonebook-back/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 morgan.token("body", (req) =>
   req.body && Object.keys(req.body).length ? JSON.stringify(req.body) : ""
 );
