ba93efc3fe30fcf7b1e0c2185a5120ac3691affa
Diego Romero <diegoromeropersonal@gmail.com>
2021-10-29 03:18:55 -0300

    one reducer

---
+++ part6/redux-anecdotes/src/AnecdoteForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { createAnecdote } from "./reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(createAnecdote(e.target.anecdote.value));
+        e.target.anecdote.value = "";
+    };
+
+    return (
+        <>
+            <h2>create new</h2>
+            <form onSubmit={handleSubmit}>
+                <div><input name="anecdote" /></div>
+                <button>create</button>
+            </form>
+        </>
+    );
+};
+
+export default AnecdoteForm;
